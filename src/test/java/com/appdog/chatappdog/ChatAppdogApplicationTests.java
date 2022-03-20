33f9c85d66604e369c6715d643d303a55095ddf7
Fidel Huarcaya <fidelhuarcaya20@gmail.com>
2022-03-20 00:49:23 -0500

    chat app-dog beta-01

---
+++ src/test/java/com/appdog/chatappdog/ChatAppdogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appdog.chatappdog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatAppdogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
